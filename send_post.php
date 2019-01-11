481319993bbd05252741a98cbdb8d5a07c5e400a
zkoyar <46579532+zkoyar@users.noreply.github.com>
2019-01-12 00:47:34 +0100

    Delete send_post.php

--- send_post.php
+++
@@ -1,8 +0,0 @@
-<form action="invio_post.php" method="POST">
-<input type="text" name="autore" placeholder="Autore(puoi lasciare vuoto se vuoi)"><br>
-<textarea rows="6" cols="60" name="messaggio">Inserisci dedica sul sito per far vedere che sei passato</textarea><br>
-<input type="text" name="img" placeholder="Se vuoi inserisci il link di un'immagine"><br>
-<input type="submit" value="send">
-</form>
-
-<?php /*include 'dediche.php';*/ ?>
